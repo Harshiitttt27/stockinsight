8a36984431943e67ae6a584a9b4409d822f54948
Harshiitttt27 <harrysethi52@gmail.com>
2025-07-04 13:19:46 +0530

    updated flow

--- theme/urls.py
+++ theme/urls.py
@@ -6,12 +6,13 @@
 #     path('register/', register_page, name='register_page'),
 # ]
 from django.urls import path
-from .views import login_page, register_page, tailwind_test
+from .views import login_page, register_page
 from .dashboard import dashboard_view
 
 urlpatterns = [
+    path('', register_page, name='register_page'),  # 👈 Root URL now shows register page
     path('login/', login_page, name='login_page'),
     path('register/', register_page, name='register_page'),
     path('dashboard/', dashboard_view, name='dashboard'),
-    path('tailwind-test/', tailwind_test, name='tailwind_test'),
+   
 ]
